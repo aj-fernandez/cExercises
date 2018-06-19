7309b31680712e5d19ce2a23e03f183b2e4bab18
aj_fernandez <agustinjfd@outlook.es>
2018-06-19 10:05:38 +0200

    Update copyArrayWithPointerWhile.cpp

--- copyArrayWithPointerWhile.cpp
+++ copyArrayWithPointerWhile.cpp
@@ -1,18 +1,18 @@
-/* Program 3.1 from PTRTUT10.HTM 6/13/97 */
+/*  PTRTUT10.HTM 7 */
 #include <stdio.h>
 char strA[80] = "Cadena a usar para el programa de ejemplo que copia una cadena en otra";
 char strB[80];
 int main(void)
 {
-char *pA; /* un apuntador al tipo caracter */
-char *pB; /* otro apuntador al tipo caracter */
+char *pA; /* un puntero al tipo caracter */
+char *pB; /* otro puntero al tipo caracter */
 puts(strA); /* muestra la cadena strA */
 pA = strA; /* apunta pA a la cadena strA */
 puts(pA); /* muestra a donde apunta pA */
 pB = strB; /* apunta pB a la cadena strB */
 printf("pB apunta a:");
 puts(pB);
-putchar('\n'); /* dejamos una línea en blanco */
+putchar('\n'); /* dejamos una lÃ­nea en blanco */
 while(*pA != '\0') /* linea A (ver texto) */
 {
 *pB++ = *pA++; /* linea B (ver texto) */
