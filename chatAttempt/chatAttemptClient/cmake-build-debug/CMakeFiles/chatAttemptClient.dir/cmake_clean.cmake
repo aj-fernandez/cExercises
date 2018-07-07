658c24c448f39b5f8fd15007825e7a56ebdeba5a
aj-fernandez <agustinjfd@outlook.es>
2018-07-07 18:17:09 +0200

    Trying to do a simple chat for linux terminal

---
+++ chatAttempt/chatAttemptClient/cmake-build-debug/CMakeFiles/chatAttemptClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chatAttemptClient.dir/main.c.o"
+  "chatAttemptClient.pdb"
+  "chatAttemptClient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chatAttemptClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chatAttempt/chatAttemptServer/cmake-build-debug/CMakeFiles/chatAttemptServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chatAttemptServer.dir/main.c.o"
+  "chatAttemptServer.pdb"
+  "chatAttemptServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chatAttemptServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
