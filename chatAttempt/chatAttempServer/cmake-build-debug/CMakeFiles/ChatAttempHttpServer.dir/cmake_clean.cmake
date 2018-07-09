9094e251d5cb25e167dec3221fad74f17bbc61bf
aj-fernandez <agustinjfd@outlook.es>
2018-07-09 20:27:05 +0200

    update

---
+++ chatAttempt/chatAttempServer/cmake-build-debug/CMakeFiles/ChatAttempHttpServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChatAttempHttpServer.dir/main.c.o"
+  "ChatAttempHttpServer.pdb"
+  "ChatAttempHttpServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ChatAttempHttpServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
